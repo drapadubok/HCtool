1ad92e8d729e716ed2e5793b980dc2767697d5b7
Dmitry Smirnov <dmi.smirnov07@gmail.com>
2015-10-16 10:22:15 +0300

    Delete initCLSpipeline.py

--- HCtool/initCLSpipeline.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-'''
-Utility to run analysis pipeline from command line.
-We submit it to slurm.
-'''
-import sys
-from HCtool.MapReduce import selectPipeline
-print 'Path to cfg:', str(sys.argv[1])
-selectPipeline(sys.argv[1])
